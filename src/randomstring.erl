f29c882f130aeb10735d986fd210be4f2f2207f7
Ben Ahlan <benahlan@yahoo.com>
2010-11-09 21:50:02 +0000

    randomstring as rebar release

---
+++ src/randomstring.erl
@@ -0,0 +1,9 @@
+-module(randomstring).
+-export([get/2]).
+
+get(Length, AllowedChars) ->
+    lists:foldl(fun(_, Acc) ->
+                        [lists:nth(random:uniform(length(AllowedChars)),
+                                   AllowedChars)]
+                            ++ Acc
+                end, [], lists:seq(1, Length)).
